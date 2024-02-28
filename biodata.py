fb7f198dc7fea4edc4074129389fd977b3df0f77
AlviRangga <142329053+HattaAlvi@users.noreply.github.com>
2024-02-28 14:50:17 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,14 @@
+nama_depan = "M. Alvi Hatta Dwi Rangga"
+age = "18"
+major = "Informatics Engineering"
+kelas = "TI23I"
+alamat = "Sukabumi"
+gender = "Man"
+quote = "Kesuksesan tidak datang kepada kita, kita yang harus menciptakannya."
+
+print("Myname is ", nama_depan)
+print("I'm a", gender)
+print("i'm", age, "Years old")
+print("My major is", major)
+print("I lived in", alamat)
+print("Kata kata hari ini: ", quote)
