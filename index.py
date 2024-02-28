d3bac042fb6c5b7ae0af4fae54c75dc010825e48
AlviRangga <alvi.hatta_ti23@nusaputra.ac.id>
2024-02-28 14:32:33 +0700

    Nambahin .py

---
+++ index.py
@@ -0,0 +1,8 @@
+panjang = 5
+lebar = 10.5
+luas = panjang * lebar
+
+print(panjang, "*", lebar, "*", luas)
+print("tipe dari variabel panjang:", type(panjang))
+print("tipe dari variabel lebar:", type(lebar))
+print("type dari variabel luas:", type(luas))
